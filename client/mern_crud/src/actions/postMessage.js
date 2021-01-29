4684c05c24d8941c7e5c80844c112070b6d02fe2
ramzi-omari <ramziomari1995@gmail.com>
2021-01-29 22:26:51 +0100

    finish crud operations

--- client/mern_crud/src/actions/postMessage.js
+++ client/mern_crud/src/actions/postMessage.js
@@ -12,11 +12,52 @@ export const fetchAll = () => (dispatch) => {
     .postMessage()
     .fetchAll()
     .then((res) => {
-      console.log(res);
       dispatch({
         type: ACTION_TYPES.FETCH_ALL,
         payload: res.data,
       });
+    })
+    .catch((err) => console.log(err));
+};
+
+export const create = (data, onSuccess) => (dispatch) => {
+  api
+    .postMessage()
+    .create(data)
+    .then((res) => {
+      dispatch({
+        type: ACTION_TYPES.CREATE,
+        payload: res.data,
+      });
+      onSuccess();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const update = (id, data, onSuccess) => (dispatch) => {
+  api
+    .postMessage()
+    .put(id, data)
+    .then((res) => {
+      dispatch({
+        type: ACTION_TYPES.UPDATE,
+        payload: res.data,
+      });
+      onSuccess();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const Delete = (id, data, onSuccess) => (dispatch) => {
+  api
+    .postMessage()
+    .delete(id)
+    .then((res) => {
+      dispatch({
+        type: ACTION_TYPES.DELETE,
+        payload: id,
+      });
+      onSuccess();
     })
     .catch((err) => console.log(err));
 };
