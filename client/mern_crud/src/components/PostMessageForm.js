def0c197b71237b8f2e0aaa19d704aed7f64e8b4
ramzi-omari <ramziomari1995@gmail.com>
2021-01-29 22:01:24 +0100

    perfect Form styling & event handling

--- client/mern_crud/src/components/PostMessageForm.js
+++ client/mern_crud/src/components/PostMessageForm.js
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { withStyles, TextField, Button } from "@material-ui/core";
+import useForm from "./useForm";
 
-const PostMessageForm = (props) => {
-  return <div>pmForm</div>;
+const initialFieldValues = {
+  title: "",
+  message: "",
 };
 
-export default PostMessageForm;
+const styles = (theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+  },
+  form: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  postBtn: {
+    width: "50%",
+  },
+});
+
+const PostMessageForm = ({ classes, ...props }) => {
+  const validate = () => {
+    // to check the inpute form validation
+    let temp = { ...errors };
+    temp.title = values.title ? "" : "this field is required";
+    temp.message = values.message ? "" : "this field is required";
+    setErrors({
+      ...temp,
+    });
+    return Object.values(temp).every((x) => x == "");
+  };
+
+  var { values, setValues, handleInputChange, errors, setErrors } = useForm(
+    initialFieldValues
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) window.alert("validation successed");
+    else window.alert("Please insert your infos");
+  };
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      className={`${classes.root} ${classes.form}`}
+    >
+      <TextField
+        name="title"
+        variant="outlined"
+        label="Title"
+        fullWidth
+        value={values.title}
+        onChange={handleInputChange}
+        {...(errors.title && { error: true, helperText: errors.title })}
+      />
+      <TextField
+        name="message"
+        variant="outlined"
+        label="Message"
+        fullWidth
+        multiline
+        rows={4}
+        value={values.message}
+        onChange={handleInputChange}
+        {...(errors.message && { error: true, helperText: errors.message })}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        type="submit"
+        className={classes.postBtn}
+        onClick={handleSubmit}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default withStyles(styles)(PostMessageForm);
---
+++ client/mern_crud/src/components/useForm.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+const useForm = (initialFieldValues) => {
+  const [values, setValues] = useState(initialFieldValues);
+  const [errors, setErrors] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  return {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+  };
+};
+
+export default useForm;
